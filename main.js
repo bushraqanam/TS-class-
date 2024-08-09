dac59f17df41eaf91ebe08704c13409e7afd7312
bushraqanam <bqanam@gmail.com>
2024-08-08 20:31:29 -0500

    updated class 8 file

---
+++ main.js
@@ -0,0 +1,85 @@
+//combainding operators//
+//assingnment operators//
+var result = 10 + 2 * 3 / 2 + (5 * 3) - 8;
+//apply bodmas rule// b for bracket,o for open, d for division,m for multiplycation, a for addition, s for subraction
+console.log(result);
+//assingnment operators//
+var num = 9;
+num += 5;
+num -= 2;
+num *= 4;
+num /= 3;
+num = Math.pow(num, 2);
+num %= 2;
+console.log(num);
+console.log(num + 7);
+//combainding operator//
+var result1 = 20 + 5 * 3 / 4 + (6 * 4) - 3;
+//apply bodmas rule//
+console.log(result1);
+//assingnment operators//
+var num1 = 20;
+num1 += 5;
+num1 -= 3;
+num1 *= 4;
+num1 %= 2;
+num1 /= 6;
+num1 = Math.pow(num1, 4);
+console.log(num1);
+console.log(num1 + 4);
+//arthmatic operators//
+var num2 = 25;
+var num3 = 12;
+var num4 = num2 + num3;
+console.log(num4);
+num4 = 40;
+console.log(num4);
+num4 = 45;
+console.log(num4);
+//subract//
+var num5 = 22;
+var num6 = 15;
+var num7 = num5 - num6;
+console.log(num7);
+num7 = 25;
+console.log(num7);
+num7 = 39;
+console.log(num7);
+//multiply//
+var num8 = 32;
+var num9 = 26;
+var num10 = num8 * num9;
+console.log(num10);
+num10 = 95 * 3;
+console.log(num10);
+num10 = 46 * 6;
+console.log(num10);
+//division//
+var num11 = 55;
+var num12 = 45;
+var num13 = num11 / num12;
+console.log(num13);
+num13 = 24 / 2;
+console.log(num13);
+num13 = 45 / 3;
+console.log(num13);
+//exponentation , mpdules//
+var A = 8;
+var power = 10;
+var result2 = Math.pow(A, power);
+console.log(result2);
+var mood = 8 % 3;
+console.log(mood);
+//uninery operators//adding//
+var abc = 34;
+console.log(++abc);
+++abc;
+console.log(abc);
+abc++;
+console.log(abc);
+--abc;
+console.log(abc);
+--abc;
+console.log(abc);
+abc--;
+console.log(abc);
---
+++ main.ts
@@ -0,0 +1,89 @@
+//combainding operators//
+//assingnment operators//
+let result:number = 10+2*3/2+(5*3)-8
+//apply bodmas rule// b for bracket,o for open, d for division,m for multiplycation, a for addition, s for subraction
+console.log(result)
+//assingnment operators//
+let num:number = 9
+num +=5
+num -=2
+num *=4
+num /=3
+num **=2
+num %=2
+console.log(num)
+console.log(num+7)
+
+//combainding operator//
+let result1:number = 20+5*3/4+(6*4)-3
+//apply bodmas rule//
+console.log(result1)
+//assingnment operators//
+let num1:number = 20
+num1 +=5
+num1 -=3
+num1 *=4
+num1 %=2
+num1 /=6
+num1 **=4
+console.log(num1)
+console.log(num1+4)
+//arthmatic operators//
+let num2 = 25
+let num3 =12
+let num4 =num2+num3
+console.log(num4)
+num4 = 40
+console.log(num4)
+num4 = 45
+console.log(num4)
+//subract//
+let num5 = 22
+let num6 = 15
+let num7 = num5 - num6
+console.log(num7)
+num7 = 25
+console.log(num7)
+num7 = 39
+console.log(num7)
+//multiply//
+let num8 = 32
+let num9 = 26
+let num10 = num8 * num9
+console.log(num10)
+num10 = 95 * 3
+console.log(num10)
+num10 = 46 * 6
+console.log(num10)
+//division//
+let num11 = 55
+let num12 = 45
+let num13 = num11 / num12
+console.log(num13)
+num13 = 24 / 2
+console.log(num13)
+num13 = 45 / 3
+console.log(num13)
+//exponentation , mpdules//
+let A = 8
+let power = 10
+let result2 = A**power
+console.log(result2)
+let mood = 8 % 3
+console.log(mood)
+//uninery operators//adding//
+let abc = 34
+console.log(++abc)
+++abc
+console.log(abc)
+abc++
+console.log(abc)
+--abc
+console.log(abc)
+--abc
+console.log(abc)
+abc--
+console.log(abc)
+
+
+
